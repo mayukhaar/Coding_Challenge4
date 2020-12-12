8995cc7df0a89f8e1f0a51f7c8b564efdc55a208
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-12-11 20:24:22 -0500

    Update TextAdventure_Coding.py

--- TextAdventure_Coding.py
+++ TextAdventure_Coding.py
@@ -1,8 +1,10 @@
+points = 0
 print("Hello there.")
 name = input("What's your name?~ ")
 chr_inv = "Character Inventory: "
 print("Hello " + str(name) + "!")
 print(str(chr_inv) + str(name))
+print(str("Points: ") + str(points + 1))
 print("What type of colors do you like?")
 cool = "a) I like cool colors!\n"
 warm = "b) I like warm colors!"
@@ -13,6 +15,7 @@ question_1 = input("a or b: ")
 if question_1 == "a" or question_1 == "A":
     print("That's great!")
     print("This is Bob: ◉_◉ . He likes cool colors too.")
+    print(str("Points: ") + str(points + 2))
     print(str(chr_inv) + str(name) + str(", Bob"))
     print("Do you want to meet more characters like Bob?")
     no_thx = "a) No thanks. I don't really feel like meeting Bob's buddies.\n"
@@ -22,6 +25,7 @@ if question_1 == "a" or question_1 == "A":
     if question_2 == "a" or question_2 == "A":
         print(question_2)
         print("Aw, maybe next time.")
+        print(str("Points: ") + str(points + 3))
         print("Do you prefer the summer or winter months?")
         winter = "a) I like the winter months. \n"
         summer = "b) I like the summer months"
@@ -29,13 +33,16 @@ if question_1 == "a" or question_1 == "A":
         question_3 = input("a or b:")
         if question_3 == "a" or question_3 == "A":
              print("Cool. Happy winter!!")
+             print(str("Points: ") + str(points + 4))
         elif question_3 == "b" or question_3 == "B":
              print("That's great, summer is amazing.")
+             print(str("Points: ") + str(points) + 4)
     elif question_2 == "b" or question_2 == "B":
         print("Yay!!")
         print("This is Bob's friend Freddie: (~˘▾˘)~")
         print("And this is Daisy: \ (•◡•) /")
         print(str(chr_inv) + str(name) + str(", Bob") + str(", Freddie") + str(", Daisy"))
+        print(str("Points: ") + str(points + 4))
         winter = "a) I like the winter months. \n"
         summer = "b) I like the summer months."
         print("Do you prefer the summer or winter months?")
@@ -43,19 +50,23 @@ if question_1 == "a" or question_1 == "A":
         question_3 = input("a or b:")
         if question_3 == "a" or question_3 == "A":
             print("Cool. Happy winter!!")
+            print(str("Points: ") + str(points + 5))
         elif question_3 == "b" or question_3 == "B":
             print("That's great, summer is amazing.")
+            print(str("Points: ") + str(points + 5))
 elif question_1 == "b" or question_1 == "B":
     no = "a) No thanks. I don't really feel like meeting Sally's buddies.\n"
     yes = "b) Sure, I would love to meet more people like Sally!"
     print("That's great!")
     print("This is Sally: (｡◕‿◕｡) .She likes warm colors too.")
     print(str(chr_inv) + str(name) + str(", Sally"))
+    print(str("Points: ") + str(points + 2))
     print("Do you want to meet more characters like Sally?")
     print(str(no) + str(yes))
     question_2 = input("a or b:")
     if question_2 == "a" or question_2 == "A":
         print("Aww, maybe next time.")
+        print(str("Points: ") + str(points + 3))
         print("Do you prefer the summer or winter months?")
         winter = "a) I like the winter months. \n"
         summer = "b) I like the summer months."
@@ -63,13 +74,16 @@ elif question_1 == "b" or question_1 == "B":
         question_3 = input("a or b:")
         if question_3 == "a" or question_3 == "A":
             print("Cool. Happy winter!!")
+            print(str("Points: ") + str(points + 4))
         elif question_3 == "b" or question_3 == "B":
             print("That's great, summer is amazing.")
+            print(str("Points: ") + str(points + 4))
     elif question_2 == "b" or question_2 == "B":
         print("Yay!!")
         print("This is Sally's sister Marcy: (◕‿◕✿) .")
         print("And this is Sally's best friend Tim: (─‿‿─) .")
         print(str(chr_inv) + str(name) + str(", Sally") + str(", Marcy") + str(", Tim"))
+        print(str("Points: ") + str(points + 5))
         print("Do you prefer the summer or winter months?")
         winter = "a) I like the winter months. \n"
         summer = "b) I like the summer months"
@@ -77,8 +91,11 @@ elif question_1 == "b" or question_1 == "B":
         question_3 = input("a or b:")
         if question_3 == "a" or question_3 == "A":
             print("Cool. Happy winter!!")
+            print(str("Points: ") + str(points + 6))
         elif question_3 == "b" or question_3 == "B":
             print("That's great, summer is amazing.")
+            print(str("Points: ") + str(points + 6))
+
 
 #Things still needed to do: -create a point system
-#                           -create a character inventory
+#-invalid
